d932fe7bea6f94eac4b3938100a1bec13ba09c23
frankB415 <117025545+frankB415@users.noreply.github.com>
2026-05-31 17:15:35 +0200

    devEditor: update schematicBlock/schematicBlock.module.js

---
+++ schematicBlock/schematicBlock.module.js
@@ -0,0 +1,7 @@
+/*
+schematicBlock.module.js — ES-Modul-Wrapper
+import { schematicBlock } from '/schematic/schematicBlock/schematicBlock.module.js'
+*/
+await import('./schematicBlock.js');
+export const schematicBlock = window.schematicBlock;
+export default window.schematicBlock;
